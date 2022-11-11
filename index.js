e47c45af725ea9a527bc4100319b7458bc2dcc72
Jorge <jcarracedohz@gmail.com>
2022-11-11 16:31:18 +0100

    Init package

---
+++ index.js
@@ -0,0 +1,16 @@
+const hola = () => {
+    console.log("Hola mundo")
+}
+
+const holaPersonalizado = (nombre) => {
+    console.log(`Hola ${nombre}`)
+}
+
+const holaAlCursoNPM = () => {
+    console.log("Hola al curso NPM")
+}
+
+modeule.exports = {
+ hola, 
+ holaPersonalizado
+}
